598b97c7b301ade994ac96d5a55e409cb547c179
Ali Osman Öztürk <developeralioz@gmail.com>
2024-03-20 18:24:27 +0300

    Update Contents.swift

--- Day 15 - Swift review/01 - Introduction.playground/Contents.swift
+++ Day 15 - Swift review/01 - Introduction.playground/Contents.swift
@@ -4,36 +4,43 @@ import Cocoa
 /// # Giriş
 
 /**
- Congratulations! You made it through all the fundamentals of the Swift programming language. Each day was purposefully very short to give you a solid introduction to the topic, but today is different because we’re going to be reviewing almost everything you learned so far.
- Tebrikler! Swift programlama dilinin tüm temellerini tamamladınız. Her gün, konuya sağlam bir giriş yapmanızı sağlamak için kasıtlı olarak çok kısaydı, ancak bugün farklı çünkü şimdiye kadar öğrendiğiniz neredeyse her şeyi gözden geçireceğiz.
- 
- I know what you’re thinking. You’re thinking “why bother? I already learned this stuff – why go over it again?”
- Ne düşündüğünüzü biliyorum. "Neden uğraşayım ki? Bunları zaten öğrendim - neden tekrar üzerinden geçeyim ki?"
+    Congratulations! You made it through all the fundamentals of the Swift programming language. Each day was purposefully very short to give you a solid introduction to the topic, but today is different because we’re going to be reviewing almost everything you learned so far.
+    Tebrikler! Swift programlama dilinin tüm temellerini tamamladınız. Her gün, konuya sağlam bir giriş yapmanızı sağlamak için kasıtlı olarak çok kısaydı, ancak bugün farklı çünkü şimdiye kadar öğrendiğiniz neredeyse her şeyi gözden geçireceğiz.
 
- Well, there are several reasons:
- Bunun birkaç nedeni var:
- - Repetition helps us learn things more thoroughly. These fundamentals are really important in all the Swift you’ll write, so it’s worth doubling down to make sure you understand them fully.
- - Tekrar, bir şeyleri daha iyi öğrenmemize yardımcı olur. Bu temel bilgiler, yazacağınız tüm Swift'ler için gerçekten önemlidir, bu nedenle bunları tam olarak anladığınızdan emin olmak için iki katına çıkmaya değer.
+    I know what you’re thinking. You’re thinking “why bother? I already learned this stuff – why go over it again?”
+    Ne düşündüğünüzü biliyorum. "Neden uğraşayım ki? Bunları zaten öğrendim - neden tekrar üzerinden geçeyim ki?"
 
- - Some concepts only make complete sense when you’ve understood other concepts. Circling back like this will help link together the core Swift concepts in your head.
- - Bazı kavramlar ancak diğer kavramları anladığınızda tam olarak anlam kazanır. Bu şekilde geri dönmek, kafanızdaki temel Swift kavramlarını birbirine bağlamanıza yardımcı olacaktır.
+    Well, there are several reasons:
+    Bunun birkaç nedeni var:
+    - Repetition helps us learn things more thoroughly. These fundamentals are really important in all the Swift you’ll write, so it’s worth doubling down to make sure you understand them fully.
+    - Tekrar, bir şeyleri daha iyi öğrenmemize yardımcı olur. Bu temel bilgiler, yazacağınız tüm Swift'ler için gerçekten önemlidir, bu nedenle bunları tam olarak anladığınızdan emin olmak için iki katına çıkmaya değer.
 
- - You might have missed something. Let’s face it, you’ve just gone through a ton of videos about the most important parts of Swift – are you absolutely sure you remembered all of it?
- - Bir şeyleri kaçırmış olabilirsiniz. Kabul edelim, Swift'in en önemli kısımları hakkında bir ton video izlediniz - hepsini hatırladığınızdan kesinlikle emin misiniz?
+    - Some concepts only make complete sense when you’ve understood other concepts. Circling back like this will help link together the core Swift concepts in your head.
+    - Bazı kavramlar ancak diğer kavramları anladığınızda tam olarak anlam kazanır. Bu şekilde geri dönmek, kafanızdaki temel Swift kavramlarını birbirine bağlamanıza yardımcı olacaktır.
 
- - You’ll see how far you’ve come. When you read about arrays and loops – things you might have found challenging previously – hopefully you’ll find them easier now, and feel good that you’re making progress.
- - Ne kadar yol kat ettiğinizi göreceksiniz. Diziler ve döngüler hakkında okuduğunuzda - daha önce zor bulmuş olabileceğiniz şeyler - umarım artık bunları daha kolay bulursunuz ve ilerleme kaydettiğiniz için kendinizi iyi hissedersiniz.
+    - You might have missed something. Let’s face it, you’ve just gone through a ton of videos about the most important parts of Swift – are you absolutely sure you remembered all of it?
+    - Bir şeyleri kaçırmış olabilirsiniz. Kabul edelim, Swift'in en önemli kısımları hakkında bir ton video izlediniz - hepsini hatırladığınızdan kesinlikle emin misiniz?
 
- And if you do find today easy, that’s okay – immediately afterwards we’ll be starting the SwiftUI projects at which point the difficulty level ramps up. Enjoy the quiet while it lasts!
- Ve eğer bugünü kolay bulursanız, sorun değil - hemen ardından SwiftUI projelerine başlayacağız ve bu noktada zorluk seviyesi yükselecek. Sessizlik sürerken tadını çıkarın!
+    - You’ll see how far you’ve come. When you read about arrays and loops – things you might have found challenging previously – hopefully you’ll find them easier now, and feel good that you’re making progress.
+    - Ne kadar yol kat ettiğinizi göreceksiniz. Diziler ve döngüler hakkında okuduğunuzda - daha önce zor bulmuş olabileceğiniz şeyler - umarım artık bunları daha kolay bulursunuz ve ilerleme kaydettiğiniz için kendinizi iyi hissedersiniz.
 
- Today you have just one tutorial to follow, although it’s pretty darn packed. This tutorial recaps almost every topic we’ve covered so far using precise code samples from the various days, and is available either as an hour-long video or one long text article.
- Bugün takip etmeniz gereken sadece bir eğitim var, ancak oldukça dolu. Bu eğitim, çeşitli günlerden kesin kod örnekleri kullanarak şimdiye kadar işlediğimiz hemen hemen her konuyu özetliyor ve bir saatlik bir video veya uzun bir metin makalesi olarak mevcut.
+    And if you do find today easy, that’s okay – immediately afterwards we’ll be starting the SwiftUI projects at which point the difficulty level ramps up. Enjoy the quiet while it lasts!
+    Ve eğer bugünü kolay bulursanız, sorun değil - hemen ardından SwiftUI projelerine başlayacağız ve bu noktada zorluk seviyesi yükselecek. Sessizlik sürerken tadını çıkarın!
 
+    Today you have just one tutorial to follow, although it’s pretty darn packed. This tutorial recaps almost every topic we’ve covered so far using precise code samples from the various days, and is available either as an hour-long video or one long text article.
+    Bugün takip etmeniz gereken sadece bir eğitim var, ancak oldukça dolu. Bu eğitim, çeşitli günlerden kesin kod örnekleri kullanarak şimdiye kadar işlediğimiz hemen hemen her konuyu özetliyor ve bir saatlik bir video veya uzun bir metin makalesi olarak mevcut.
+    - Here’s the tutorial you need to follow today: Learn essential Swift in one hour. : https://www.hackingwithswift.com/articles/242/learn-essential-swift-in-one-hour
+    - İşte bugün takip etmeniz gereken eğitim: Bir saat içinde temel Swift'i öğrenin. : https://www.hackingwithswift.com/articles/242/learn-essential-swift-in-one-hour
 
+    Optional: once you’ve finished reviewing that article, you can go back to the review page and go over any tests related to the topics above that you want  -  https://www.hackingwithswift.com/review
+    İsteğe bağlı: Bu makaleyi gözden geçirmeyi tamamladıktan sonra, gözden geçirme sayfasına geri dönebilir ve yukarıdaki konularla ilgili istediğiniz testlerin üzerinden geçebilirsiniz - https://www.hackingwithswift.com/review
 
- Here’s the tutorial you need to follow today: Learn essential Swift in one hour.
- Optional: once you’ve finished reviewing that article, you can go back to the review page and go over any tests related to the topics above that you want – it’s your choice/
- 
- 
+    Bonus: find the Swifty words!
+    I’ve made a word search to help you test your knowledge of the most useful terms for Swift developers. You can download it as a PDF here, and if you need help you can try looking through my Swift glossary.
+    Swift geliştiricileri için en yararlı terimler hakkındaki bilginizi test etmenize yardımcı olmak için bir kelime araması yaptım. PDF olarak buradan indirebilir ve yardıma ihtiyacınız olursa Swift sözlüğüme bakmayı deneyebilirsiniz.
+    https://www.hackingwithswift.com/files/100/15-wordsearch.pdf
+    https://www.hackingwithswift.com/glossary
+
+    There are dozens to find, and words can go in all directions – good luck!
+    Bulunacak düzinelerce şey var ve kelimeler her yöne gidebilir - iyi şanslar!
 */
