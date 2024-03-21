e34ff242db96912edc4afd7556a4df2995bdac8f
Ali Osman Öztürk <developeralioz@gmail.com>
2024-03-21 11:13:01 +0300

    Update Contents.swift

--- Day 15 - Swift review/02 - Learn essential Swift in one hour.playground/Contents.swift
+++ Day 15 - Swift review/02 - Learn essential Swift in one hour.playground/Contents.swift
@@ -1250,7 +1250,55 @@ func printSquare(of number: Int?) {
 
 /// # Nil coalescing
 
+/// Swift has a third way of unwrapping optionals, called the nil coalescing operator – it unwraps an optional and provides a default value if the optional was empty:
+/// Swift, nil birleştirme operatörü olarak adlandırılan üçüncü bir opsiyonel açma yöntemine sahiptir - bir opsiyonel açar ve opsiyonel boşsa varsayılan bir değer sağlar:
+
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+let favorite = tvShows.randomElement() ?? "None"
+
+/// The nil coalescing operator is useful in many places optionals are created. For example, creating an integer from a string returns an optional Int? because the conversion might have failed. Here we can use nil coalescing to provide a default value:
+/// nil birleştirme operatörü, opsiyonellerin oluşturulduğu birçok yerde kullanışlıdır. Örneğin, bir string'ten bir integer oluşturmak, dönüştürme başarısız olabileceğinden bir optional Int? döndürür. Burada, varsayılan bir değer sağlamak için nil coalescing kullanabiliriz:
+
+let input = ""
+let number3 = Int(input) ?? 0
+print(number3)
 
 
+// ****************************************************************************
 
 
+/// # Optional chaining
+
+/// Optional chaining reads optionals inside optionals, like this:
+/// Opsiyonel zincirleme, opsiyonelleri opsiyonellerin içinde okur, bunun gibi:
+
+let names = ["Arya", "Bran", "Robb", "Sansa"]
+let chosen = names.randomElement()?.uppercased()
+print("Next in line: \(chosen ?? "No one")")
+
+/// Optional chaining is there on line 2: a question mark followed by more code. It allows us to say “if the optional has a value inside, unwrap it then…” and add more code. In our case we’re saying “if we got a random element from the array, uppercase it.”
+/// Optional chaining 2. satırda var: bir soru işareti ve ardından daha fazla kod. "Eğer opsiyonun içinde bir değer varsa, onu aç ve sonra..." dememizi ve daha fazla kod eklememizi sağlar. Bizim durumumuzda "eğer diziden rastgele bir eleman aldıysak, onu büyük harfle yaz" diyoruz.
+
+
+// ****************************************************************************
+
+
+/// # Optional try?
+
+/// When calling a function that might throw errors, we can use try? to convert its result into an optional containing a value on success, or nil otherwise.
+/// Hata verebilecek bir fonksiyonu çağırırken, try? seçeneğini kullanarak fonksiyonun sonucunu başarı durumunda bir değer içeren bir opsiyonel değere, aksi takdirde nil değerine dönüştürebiliriz.
+
+enum UserError: Error {
+    case badID, networkFailed
+}
+
+func getUser(id: Int) throws -> String {
+    throw UserError.networkFailed
+}
+
+if let user = try? getUser(id: 23) {
+    print("User: \(user)")
+}
+
+/// The getUser() function will always throw networkFailed, but we don’t care what was thrown – all we care about is whether the call sent back a user or not.
+/// getUser() fonksiyonu her zaman networkFailed atacaktır, ancak neyin atıldığı umurumuzda değildir - tek umursadığımız çağrının bir kullanıcıyı geri gönderip göndermediğidir.
