3d7e6f508755e028d5e6993a4688615199566b42
Ali Osman Öztürk <developer@aliosmanozturk.me>
2024-04-22 22:13:27 +0300

    Day 16 - Introduction

---
+++ Day 16 - Project 1, part one/01 - Introduction.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+/// # Introduction
+/// # Giriş
+
+/**
+    Now that you’ve mastered the basics of the Swift language, it’s time to start applying your skills to some real code in our first project.
+    Artık Swift dilinin temellerini öğrendiğinize göre, ilk projemizde becerilerinizi bazı gerçek kodlara uygulamaya başlamanın zamanı geldi.
+
+    This project is a check-sharing app that calculates how to split a check based on the number of people and how much tip you want to leave. The project in itself isn’t complicated, but we’ll be taking it slow so you can see exactly how these fundamentals fit together.
+    Bu proje, kişi sayısına ve ne kadar bahşiş bırakmak istediğinize göre bir çeki nasıl bölüştüreceğinizi hesaplayan bir çek paylaşım uygulamasıdır. Proje kendi içinde karmaşık değil, ancak bu temellerin birbirine nasıl uyduğunu tam olarak görebilmeniz için yavaş ilerleyeceğiz.
+
+    In some respects going back to the basics like this will feel odd – you’ve learned about closures, optionals, and throwing functions, and now there’s a bit of a reset as we look at the basics of SwiftUI. But take hope: there’s a lot of value in being able to approach new topics with a fresh mind. As Meister Eckhart said, “be willing to be a beginner every single morning” – do that, and you’ll learn much faster.
+    Bazı açılardan bu şekilde temellere geri dönmek garip gelecektir - closure'lar, optional'lar ve throwing fonksiyonları hakkında bilgi edindiniz ve şimdi SwiftUI'nin temellerine bakarken biraz sıfırlama var. Ancak umutlu olun: yeni konulara taze bir zihinle yaklaşabilmenin çok değeri var. Meister Eckhart'ın dediği gibi, "her sabah yeni başlayan biri olmaya istekli olun" - bunu yaparsanız çok daha hızlı öğrenirsiniz.
+
+    Today is the project overview day, which is where we’ll be looking at the isolated pieces of code that you need to understand in order to build our project. Tomorrow we’ll move on to the implementation day, where you’ll put those new techniques into practice with our app.
+    Bugün, projemizi oluşturmak için anlamanız gereken izole kod parçalarına bakacağımız projeye genel bakış günüdür. Yarın ise bu yeni teknikleri uygulamamızla hayata geçireceğiniz uygulama gününe geçeceğiz.
+
+    Today you have seven topics to work through, and you’ll meet Form, NavigationStack, @State, and more.
+    Bugün üzerinde çalışmanız gereken yedi konu var ve Form, NavigationStack, @State ve daha fazlasıyla tanışacaksınız.
+*/
+
