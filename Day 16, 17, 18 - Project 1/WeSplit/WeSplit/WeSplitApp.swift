12c8f0c4149e647ba52edfb406e8e7b674a9667a
Ali Osman Öztürk <developer@aliosmanozturk.me>
2024-04-23 09:45:29 +0300

    Day 16, 17, 18 - Project 1

---
+++ Day 16, 17, 18 - Project 1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ali Osman Öztürk on 22.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
