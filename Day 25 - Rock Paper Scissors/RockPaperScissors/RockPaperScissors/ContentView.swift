a2d30688ce19c0b4b8836874206e9ca69e404730
Ali Osman Öztürk <developer@aliosmanozturk.me>
2024-05-01 22:47:50 +0300

    Day 25 - Rock Paper Scissors -
    not completed

--- Day 25 - Rock Paper Scissors/RockPaperScissors/RockPaperScissors/ContentView.swift
+++ Day 25 - Rock Paper Scissors/RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -1,95 +1,69 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingScore = false
-    @State private var showingScoreAndTrueFlag = false
-    
-    @State private var tappedButton = 0
-    @State private var tappedButtonState = false
-    
-    @State private var falseFlag = 0
-    
+    @State private var situations = ["👊", "✋", "✌️"]
+    @State private var correctAnswer = Int.random(in: 0 ... 2)
+
     @State private var scoreTitle = ""
-
     @State private var userScore = 0
-
-    @State private var countries = [
-        "Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK",
-        "Ukraine", "US",
-    ].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var falseSituation = 0
+    
+    @State private var showingScore = false
+    @State private var showingScoreAndTrueSituation = false
 
     var body: some View {
         ZStack {
-            RadialGradient(
-                stops: [
-                    .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                    .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
-                ], center: .top, startRadius: 200, endRadius: 400
-            )
-            .ignoresSafeArea()
+            Color.cyan
+            
             VStack {
                 Spacer()
-                Text("Guess the Flag")
+                Text("Rock Paper Scissors")
                     .font(.largeTitle.weight(.bold))
                     .foregroundStyle(.white)
+                
                 Spacer()
+                
+                VStack {
+                    Text("Select a situation and start the game")
+                        .font(.subheadline.weight(.heavy))
+                        .foregroundStyle(.secondary)
+                }
+                
                 Spacer()
-                Text("Score: \(userScore)")
-                    .foregroundStyle(.white)
-                    .font(.title.bold())
-                Spacer()
-                VStack(spacing: 15) {
-                    VStack {
-                        Text("Tap the flag of")
-                            .font(.subheadline.weight(.heavy))
-                            .foregroundStyle(.secondary)
-                        Text(countries[correctAnswer])
-                            .font(.largeTitle.weight(.semibold))
-                    }
-
+                
+                HStack {
                     ForEach(0 ..< 3) { number in
                         Button {
-                            flagTapped(number)
+                            situationTapped(number)
                         } label: {
-                            Image(countries[number])
-                                .clipShape(.capsule)
-                                .shadow(radius: 5)
+                            Text(situations[number])
+                                .font(.system(size: 65))
+                                .padding()
+                                .background(.ultraThinMaterial)
+                                .clipShape(.rect(cornerRadius: 20))
                         }
                     }
                 }
-                .frame(maxWidth: .infinity)
-                .padding(.vertical, 20)
-                .background(.regularMaterial)
-                .clipShape(.rect(cornerRadius: 20))
+                .padding()
+                
+                Spacer()
             }
-            .padding()
         }
+        .ignoresSafeArea()
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
             Text("Your score is \(userScore)")
         }
-        .alert(scoreTitle, isPresented: $showingScoreAndTrueFlag) {
+        .alert(scoreTitle, isPresented: $showingScoreAndTrueSituation) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is \(userScore)\n" + "Wrong! That's the flag of \(countries[falseFlag]) ")
-        }
-        .alert("Game Finished", isPresented: $tappedButtonState) {
-            Button("Okey", action: resetGame)
-        } message: {
-            Text("Your final score is \(userScore)")
+            Text("Your score is \(userScore)\n" + " \(situations[falseSituation]) ")
         }
     }
-
-    func flagTapped(_ number: Int) {
-        tappedButton = tappedButton + 1
-        
-        if tappedButton >= 3 {
-            tappedButtonState = true
-        }
-        
-        
+    
+    func situationTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
             userScore = userScore + 1
@@ -99,25 +73,18 @@ struct ContentView: View {
             scoreTitle = "Wrong"
             userScore = userScore - 1
             
-            falseFlag = number
-
-            showingScoreAndTrueFlag = true
+            falseSituation = 0
+            
+            showingScoreAndTrueSituation = true
             if userScore < 0 {
                 userScore = 0
             }
         }
     }
-    
-    func resetGame() {
-        tappedButton = 0
-        userScore = 0
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
-    }
-
+      
     func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        situations.shuffle()
+        correctAnswer = Int.random(in: 0 ... 2)
     }
 }
 
